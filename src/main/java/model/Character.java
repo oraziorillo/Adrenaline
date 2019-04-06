fdd5a1ef728f264542e725d3b33a46b41a3ccf52
alesnow97 <aleruss97@gmail.com>
2019-04-06 17:45:48 +0200

    Modifica character

--- src/main/java/model/Character.java
+++ src/main/java/model/Character.java
@@ -1,13 +1,13 @@
 package model;
 
 public class Character {
-    private CharColour[] damage=new CharColour[12];
-    private Weapon[] weapons=new Weapon[3];
-    private Powerup[] powerups=new Powerup[3];
-    private CharColour[][] marks;
+    private CharColour[] damage;
+    private Weapon[] weapons;
+    private Powerup[] powerups;
+    private short[] marks;
     private int points;
     private short numOfDeath;
-    private short ammos[]=new short[3];
+    private short ammos[];
     private Tile position;
     private CharColour colour;
 
@@ -15,7 +15,7 @@ public class Character {
         this.damage = new CharColour[12];
         this.weapons = new Weapon[3];
         this.powerups = new Powerup[3];
-        this.marks = new short[4];       //l'inizializzazione di ogni giocatore avviene dopo aver definito il numero totale di giocatori
+        this.marks = new short[4];
         this.points = 0;
         this.numOfDeath = 0;
         this.ammos = new short[3];
@@ -25,6 +25,11 @@ public class Character {
 
     public void addDamage(CharColour colour, int damage, int marks){
         //TODO: method
+        int index = 0;
+        while (damage[i] != null){
+            i = i + 1;
+        }
+
     }
     public void collectWeapon(Weapon w){
         //TODO: method
@@ -38,16 +43,23 @@ public class Character {
     }
     public void addPoints(int points){
         this.points+=points;
-    }
+    }       //TODO: qui il controller dovrebbe fare dei controlli per vedere se è morto
+
     public void collectAmmos(short[] ammos){
         if(ammos.length!=this.ammos.length){
             throw new IllegalArgumentException("Array di dimensione errata");
         }
-        for(int i=0;i<ammos.length;i++){
+        for(int i=0; i<ammos.length; i++){
             this.ammos[i]+=ammos[i];
             if(ammos[i]>3)
                 ammos[i]=3;
         }
+    }
+
+    public void respawn(RoomColour colour){
+        this.damage = new CharColour[12];
+        numOfDeath = numOfDeath + 1;
+        this.position = //TODO: è dato dal colour che viene passato come parametro
     }
 
 }
