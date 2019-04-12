7448df47391262dfa17464d2e47ef3e32d624c23
oraziorillo <oraziorillo@gmail.com>
2019-04-12 19:24:33 +0200

    ADD MapIterator, UPDATE Action (better implementation), FIXes

---
+++ src/test/java/model/MapIteratorTest.java
@@ -0,0 +1,27 @@
+package model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MapIteratorTest {
+    private Tile[][] map;
+    private Game game;
+    private Deck<WeaponCard> deck;
+
+    @Before
+    public void setup(){
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                map[i][j] = new SpawnTile(j, i, TileColourEnum.RED, deck, game);
+    }
+
+    @Test
+    public void printMap(){
+        Tile currTile = map[0][0];
+        for (MapIterator itr = new MapIterator(map[0][0]); itr.hasNext();) {
+            System.out.println("(" + currTile.getX() + "," + currTile.getY() + ")");
+            currTile = itr.next();
+        }
+        System.out.println("(" + currTile.getX() + "," + currTile.getY() + ")");
+    }
+}
