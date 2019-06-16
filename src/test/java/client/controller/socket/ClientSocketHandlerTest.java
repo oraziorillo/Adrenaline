eb1ba9168f8e5afe0fea96dbf68d9dadcef3a848
Matteo <matteo.secco97@gmail.com>
2019-06-16 16:22:08 +0200

    UPDATE ClientSocketHandlerTest (WorkInProgress, must make able to change socket door)

--- src/test/java/client/controller/socket/ClientSocketHandlerTest.java
+++ src/test/java/client/controller/socket/ClientSocketHandlerTest.java
@@ -1,8 +1,14 @@
 package client.controller.socket;
 
+import common.remote_interfaces.RemoteView;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.internal.matchers.Any;
+import org.mockito.junit.MockitoJUnitRunner;
 import server.LaunchServer;
 
 import java.io.IOException;
@@ -10,27 +16,43 @@ import java.net.Socket;
 import java.util.UUID;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ClientSocketHandlerTest {
     
+    private Thread server;
     private ClientSocketHandler tested;
+    @Mock private RemoteView view;
     
     
     @Before
-    public void setUp() throws IOException {
+    public void setUp() throws IOException, ClassNotFoundException {
+        server = new Thread( ()-> {
+            try {
+                LaunchServer.main( null );
+            } catch ( IOException e ) {
+                e.printStackTrace();
+            } catch ( ClassNotFoundException e ) {
+                e.printStackTrace();
+            }
+        } );
+        server.start();
         tested = new ClientSocketHandler( new Socket("localhost",10000) );
+        new Thread( tested ).start();
+        Mockito.doNothing().when( view).ack( any(String.class) );
     }
     
     @Test
     public void registerReturnsUUIDWithServerOnAndNewUsername() throws IOException {
-        UUID returned = tested.register( "username",null );
+        UUID returned = tested.register( "username",view );
         assertNotNull( returned );
     }
     
     @Test
     public void registerReturnsNullWithInUseUsername() throws IOException {
-        UUID first = tested.register( "username",null );
-        UUID second = tested.register( "username" ,null);
+        UUID first = tested.register( "username",view );
+        UUID second = tested.register( "username" ,view);
         assertNull( second );
     }
 }
