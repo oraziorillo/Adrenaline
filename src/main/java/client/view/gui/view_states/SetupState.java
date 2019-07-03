c27270bf4c252b310d43fdc70e68875664924e82
Matteo <matteo.secco97@gmail.com>
2019-07-03 02:40:23 +0200

    HUGE guiview refactor (so much more understandable)

--- src/main/java/client/view/gui/view_states/SetupState.java
+++ src/main/java/client/view/gui/view_states/SetupState.java
@@ -7,35 +7,30 @@ import common.events.lobby_events.LobbyEvent;
 import common.events.pc_board_events.PcBoardEvent;
 import common.events.pc_events.PcEvent;
 import common.events.square_events.SquareEvent;
-import common.remote_interfaces.RemotePlayer;
-import javafx.application.HostServices;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.rmi.RemoteException;
-import java.util.List;
+import java.util.HashSet;
 
 public class SetupState extends ViewState {
    
-   private transient GameBoardEvent mapSelection;
+   private transient HashSet<GameBoardEvent> gameBoardToPass = new HashSet<>();
+   private transient HashSet<PcEvent> pcToPass = new HashSet<>();
+   private transient HashSet<KillShotTrackEvent> killShotToPass = new HashSet<>();
    private transient DoubleProperty stillChoosing = new SimpleDoubleProperty( Double.NEGATIVE_INFINITY );
    private transient DoubleProperty beforeMyTurn = new SimpleDoubleProperty( Double.NEGATIVE_INFINITY );
    
-   SetupState(Stage stage, RemotePlayer player, HostServices hostServices, List<String> previousAcks) throws RemoteException {
-      super( stage, player, hostServices, previousAcks);
+   SetupState() throws RemoteException {
+      super();
       try {
          FXMLLoader loader = new FXMLLoader( Map.class.getResource( "/fxml/gameSetup/setup.fxml" ) );
          Parent root = loader.load();
-         javafxController = loader.getController();
-         javafxController.setPlayer( player );
-         javafxController.setHostServices( hostServices );
-         for (String ack : previousAcks)
-            javafxController.ack( ack );
+         setJavafxController( loader.getController() );
          stage.setScene( new Scene( root ) );
          stage.show();
       }catch ( IOException e ){
@@ -44,42 +39,44 @@ public class SetupState extends ViewState {
          throw e1;
       }
       stillChoosing.addListener( (obs, oldV, newV) -> {
-         if (newV.intValue() == 0) {
+         if (newV.intValue() == 0)
             topView.nextState();
-            try {
-               topView.onGameBoardUpdate( mapSelection );
-            } catch ( RemoteException ignored ) {}
-         }
       } );
-      beforeMyTurn.addListener( javafxController );
+      beforeMyTurn.addListener( getJavafxController() );
    }
    
    
    @Override
    public void ack(String msg) {
-      javafxController.ack( msg );
+      getJavafxController().ack( msg );
    }
    
    @Override
    public ViewState nextState() throws RemoteException {
-      return new InGameState(stage,player,hostServices, previousAcks );
+      InGameState returned = new InGameState();
+      for(GameBoardEvent e: gameBoardToPass) returned.onGameBoardUpdate( e );
+      for (PcEvent e:pcToPass) returned.onPcUpdate( e );
+      for(KillShotTrackEvent e:killShotToPass) returned.onKillShotTrackUpdate( e );
+      return new InGameState();
    }
    
    @Override
    public void onKillShotTrackUpdate(KillShotTrackEvent event) {
-      javafxController.onKillShotTrackUpdate( event );
+      getJavafxController().onKillShotTrackUpdate( event );
+      killShotToPass.add( event );
    }
    
    @Override
    public void onPcUpdate(PcEvent event) {
-      javafxController.onPcUpdate( event );
+      getJavafxController().onPcUpdate( event );
       stillChoosing.set( stillChoosing.get()-1 );
       beforeMyTurn.set( beforeMyTurn.get()-1 );
+      pcToPass.add( event );
    }
    
    @Override
    public void onGameBoardUpdate(GameBoardEvent event) {
-      this.mapSelection = event;
+      gameBoardToPass.add( event );
    }
    
    @Override
